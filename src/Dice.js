0a810f862a301a97872f9dbb6c510514df419c20
Nickita Danilovich <nickitadanilovich@gmail.com>
2025-02-27 17:09:39 -0500

    setup most of the pages

---
+++ src/Dice.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Nav from "./Nav"
+
+function Dice (){
+    return (
+        <div>
+            <Nav />
+            <h1>This is the dice game</h1>
+        </div>
+
+    );
+}
+
+export default Dice
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from "./Nav"
+
+function Home(){
+    return (
+        <div>
+            <Nav />
+            <h1>This is the home page</h1>
+        </div>
+    );
+}
+
+export default Home;
